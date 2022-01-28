7c3c55cd12f6b358b54dc42106cedd4a77d7a986
Aurélio Soares <95449811+aurelio-sc@users.noreply.github.com>
2022-01-28 14:22:47 -0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -4,18 +4,31 @@ const range = document.querySelector(".range");
 const priceNumber = document.querySelector(".priceNumber");
 const billingPeriod = document.querySelector(".billingPeriod");
 
+const yearly = document.querySelector(".yearly");
+let rate = 1;
+
 
 viewsNumber.textContent = range.value;                            //essas linhas definem os valores iniciais.
-priceNumber.textContent = "$" + (0.16*range.value).toFixed(2);
+priceNumber.textContent = "$" + (rate*0.16*range.value).toFixed(2);
 billingPeriod.textContent = " /month";
 
 
 range.addEventListener("mouseup", function(){
     viewsNumber.textContent = range.value;
-    priceNumber.textContent = "$" + (0.16*range.value).toFixed(2);
+    priceNumber.textContent = "$" + (rate*0.16*range.value).toFixed(2);
 });
 
 range.addEventListener("touchend", function(){
     viewsNumber.textContent = range.value;
-    priceNumber.textContent = "$" + (0.16*range.value).toFixed(2);
+    priceNumber.textContent = "$" + (rate*0.16*range.value).toFixed(2);
 });
+
+yearly.addEventListener("click", function(){
+    console.log("click")
+    if (yearly.checked == true) {
+        rate = 0.75;
+    }else{
+        rate = 1;
+    }
+    priceNumber.textContent = "$" + (rate*0.16*range.value).toFixed(2);
+})
